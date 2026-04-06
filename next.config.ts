50c79cac4959ae82caabd944c356cbdc346f5414
qalqa-dev <andreybas04@gmail.com>
2026-04-07 00:35:25 +0300

    dev: installed svgr library

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
