642c5e7f860c1e51e69687a243a6705f77b91afb
qalqa-dev <113594321+qalqa-dev@users.noreply.github.com>
2026-04-07 11:18:00 +0300

    Feature/Infinite loading list (#6)
    * feat: added infinite scroll
    
    * fix: fixed styles

--- src/entities/cat/api/cat-api.ts
+++ src/entities/cat/api/cat-api.ts
@@ -2,6 +2,6 @@ import { apiClient } from '@/shared/api';
 import type { Cat } from '../model/types';
 
 export const catApi = {
-  getCats: (limit = 10) =>
-    apiClient.get<Cat[]>(`/images/search?limit=${limit}`),
+  getCats: (limit = 10, page = 0) =>
+    apiClient.get<Cat[]>(`/images/search?limit=${limit}&page=${page}`),
 };
--- src/entities/cat/index.ts
+++ src/entities/cat/index.ts
@@ -1,3 +1,4 @@
+export { loadMoreCatsAction } from './api/actions';
 export { catApi } from './api/cat-api';
 export type { Cat } from './model/types';
 export { CatCard } from './ui/CatCard';
